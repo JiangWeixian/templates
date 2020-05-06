e1f8dc51cfcacfb561cfc3bb5e2ba264ed7317e1
JiangWeixian <saber_wei@163.com>
2020-05-06 17:24:30 +0800

    🔥 (packages): remove docs template

--- packages/core/src/utils/constants.ts
+++ packages/core/src/utils/constants.ts
@@ -3,7 +3,6 @@ export const SCOPE = 'aiou'
 export const enum TEMPLATES {
   ROLLUP_STARTER = 'rollup-template',
   REACT_PC_SPA_STARTER = 'react-template',
-  BLOG_STARTER = 'docs-templates',
   CHROME_EXTENSTION_STARTER = 'chrome-extenstion-template',
   BIN_TEMPLATE = 'bin-template',
   REACT_COMPONENTS_LIB_TEMPLATE = 'react-components-lib-template',
@@ -17,7 +16,6 @@ export const enum TEMPLATES {
 export const templates = {
   [TEMPLATES.ROLLUP_STARTER]: 'rollup stater build pure function lib',
   [TEMPLATES.REACT_PC_SPA_STARTER]: 'webapp starter base on react react-router rematch',
-  [TEMPLATES.BLOG_STARTER]: 'blog stater base on vuepress',
   [TEMPLATES.CHROME_EXTENSTION_STARTER]: 'chrome-extenstion starter base ts rxjs react',
   [TEMPLATES.BIN_TEMPLATE]: 'bin template',
   [TEMPLATES.REACT_COMPONENTS_LIB_TEMPLATE]:
