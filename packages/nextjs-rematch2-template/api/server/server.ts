0c00a496301df36dc3abf87c8b82752afbc5f0d5
JiangWeixian <saber_wei@163.com>
2020-05-06 17:49:58 +0800

    🔧 (server): baseurl

--- packages/nextjs-rematch2-template/api/server/server.ts
+++ packages/nextjs-rematch2-template/api/server/server.ts
@@ -2,7 +2,7 @@ import axios from 'axios'
 import { HOST } from '~/interfaces'
 
 export const server = axios.create()
-server.defaults.baseURL = HOST.SERVER
+server.defaults.baseURL = process.env.NODE_ENV === 'development' ? HOST.MOCK : HOST.SERVER
 server.interceptors.response.use(
   async res => {
     return res.data
